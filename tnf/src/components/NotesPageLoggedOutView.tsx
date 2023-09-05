0123c43c553ef6da256eb6294552f7c496d2089c
asuquo91 <136271445+asuquo91@users.noreply.github.com>
2023-09-05 14:39:42 +0100

    updated mobile

--- tnf/src/components/NotesPageLoggedOutView.tsx
+++ tnf/src/components/NotesPageLoggedOutView.tsx
@@ -5,6 +5,7 @@ const NotesPageLoggedOutView = () => {
     return ( 
         <>
         <img className="homepagepic" src="/img/noters-homepage1.png" alt="Welcome to Noters.site" />
+        <img className="homepagepicmobile" src="/img/noters-homepage1-mobile.png" alt="Welcome to Noters.site" />
         </>
      );
 }
